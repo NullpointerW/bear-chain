106663db92dc806f6500fc16a0065711a785a395
NullpointerW <825708370@qq.com>
2024-06-11 19:35:40 +0800

    v2

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ var (
 func drip() (time.Duration, error) {
 	cli := yescaptcha.NewClient(apiKey, "33989", "https://api.yescaptcha.com")
 	task := req.TurnstileTaskProxylessRequest{
-		WebsiteURL: "https://artio.faucet.berachain.com",
+		WebsiteURL: "https://bartio.faucet.berachain.com/",
 		Type:       "TurnstileTaskProxylessM1",
 		WebsiteKey: "0x4AAAAAAARdAuciFArKhVwt",
 	}
@@ -39,9 +39,8 @@ func drip() (time.Duration, error) {
 	}
 	authorization := "Bearer " + result.Solution.Token
 	log.Println(authorization)
-	//url := "https://artio-80085-faucet-api-recaptcha.berachain.com/api/claim?address=0x961dfB987266e3D5029713E7af4F989a41eB961A"
-	url := "https://artio-80085-faucet-api-cf.berachain.com/api/claim?address=0x961dfB987266e3D5029713E7af4F989a41eB961A"
-	marshal, err := json.Marshal(map[string]string{"address": "0x961dfB987266e3D5029713E7af4F989a41eB961A"})
+	url := "https://bartio-faucet.berachain-devnet.com/api/claim?address={addr}"
+	marshal, err := json.Marshal(map[string]string{"address": "{addr}"})
 	if err != nil {
 		return 0, (err)
 	}
@@ -49,14 +48,14 @@ func drip() (time.Duration, error) {
 	if err != nil {
 		return 0, (err)
 	}
-	request.Header.Set("authority", "artio-80085-faucet-api-recaptcha.berachain.com")
+	request.Header.Set("authority", "bartio-faucet.berachain-devnet.com")
 	request.Header.Set("accept", "*/*")
 	request.Header.Set("accept-language", "zh-CN,zh;q=0.9")
 	request.Header.Set("cache-control", "no-cache")
 	request.Header.Set("content-type", "text/plain;charset=UTF-8")
-	request.Header.Set("origin", "https://artio.faucet.berachain.com")
+	request.Header.Set("origin", "https://bartio.faucet.berachain.com")
 	request.Header.Set("pragma", "no-cache")
-	request.Header.Set("referer", "https://artio.faucet.berachain.com/")
+	request.Header.Set("referer", "https://bartio.faucet.berachain.com/")
 	request.Header.Set("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36")
 	request.Header.Set("Authorization", authorization)
 	httpCli := (&http.Client{})
